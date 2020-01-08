d23d22f4395c79b35278d74b22d808390af076fe
Yx1aoq1 <704315493@qq.com>
2020-01-08 22:41:16 +0800

    Site updated: 2020-01-08 22:41:15

--- app.js
+++ app.js
@@ -1,21 +1,9 @@
-var spawn = require('child_process').spawn;
-
-free = spawn('hexo', ['server', '-d']);/* 其实就是等于执行hexo server -p 4000*/
-
-free.stdout.on('data', function (data) {
-
-    console.log('standard output:\n' + data);
-
-});
-
-free.stderr.on('data', function (data) {
-
-    console.log('standard error output:\n' + data);
-
-});
-
-free.on('exit', function (code, signal) {
-
-    console.log('child process eixt ,exit:' + code);
-
-});
+const { exec } = require('child_process')
+exec('hexo server -d',(error, stdout, stderr) => {
+        if(error){
+                console.log('exec error: ${error}')
+                return
+        }
+        console.log('stdout: ${stdout}');
+        console.log('stderr: ${stderr}');
+})
