4ac72460161155a6bb5804f61b5cf3b5eef99668
yanxq <yanxq@wangsu.com>
2020-06-30 17:56:17 +0800

    feat: add post

--- test.js
+++ test.js
@@ -1,87 +1,75 @@
-function randomInt (min, max) {
-  return Math.round(Math.random() * (max - min)) + min
-}
-
-function randomArr (len) {
-  let arr = []
-  for (let i = 0; i < len; i ++) {
-    arr.push(randomInt(0, 15))
+class Folder {
+  constructor (name) {
+    this.name = name
+    this.parent = null
+    this.files = []
   }
-  return arr
-}
-
-function swap (arr, i, j) {
-  const temp = arr[i]
-  arr[i] = arr[j]
-  arr[j] = temp
-}
-
-// 在原数组上操作的版本，比较节约空间
-function partition (arr, start, end) {
-  // 基准为数组的始元素
-  const pivot = arr[start]
-  let left = start
-  let right = end + 1
-  let cur = start + 1
-  // 从第二个元素开始比较
-  for (; cur < right; cur ++) {
-    if (arr[cur] < pivot) { 
-      // 如果当前元素比基数小，就和右边的大数交换，left就往右移了一位
-      // 可以看出最坏的情况，相当于冒泡
-      swap(arr, cur, ++ left)
-    }
-  }
-  // 把基数放在本应属于他的位置
-  swap(arr, start, left)
-  return left
-}
-
-function quickSort (arr, start, end) {
-  start = typeof start !== 'number' ? 0 : start
-  end = typeof end !== 'number' ? arr.length - 1 : end
-  if (start < end) {
-  	const partitionIndex = partition(arr, start, end)
-    quickSort(arr, start, partitionIndex - 1)
-    quickSort(arr, partitionIndex + 1, end)
-  }
-  return arr
-}
-
-function logger (arr) {
-  arr.map((item, index) => {
-    console.log(`${index}:`, item)
-  })
-}
-
-function bucketSort (arr, bucketSize = 5) {
-  const buckets = [...new Array(bucketSize)].map(() => []) // 生成一个bucketSize * bucketSize的数组空间
-  const max = Math.max(...arr)
   
-  for (let i = 0; i < arr.length; i ++) {
-    const number = arr[i]
-    const bucketIndex = Math.floor(number / (max + 1) * bucketSize) // 映射函数
-    const bucket = buckets[bucketIndex]
-    bucket.push(number)
-
-    let j = bucket.length - 1
-    // 对加进桶内的元素排序 这里的排序是普通的冒泡
-    while (j > 0 && bucket[j - 1] > bucket[j]) {
-      swap(bucket, j - 1, j)
-      j --
-    }
+  add (file) {
+    file.parent = this // 设置父对象引用
+    this.files.push(file)
   }
-  let i = 0
-  for (let bucketIndex = 0; bucketIndex < bucketSize; bucketIndex ++) {
-    const bucket = buckets[bucketIndex]
-    for (let j = 0; j < bucket.length; j ++) {
-      arr[i] = bucket[j]
-      i ++
-    }
+  
+  scan () {
+    console.log(`开始扫描文件夹：${this.name}`)
+    this.files.forEach(file => {
+      file.scan()
+    })
   }
-  return arr
+  
+  remove () {
+    if (!this.parent) return
+    const files = this.parent.files
+    files.forEach((file, index) => {
+      if (file === this) {
+        files.splice(index, 1)
+      }
+    })
+  }
 }
 
-const array = randomArr(10)
-console.log('befor sort: ', array)
-const res = bucketSort(array, 5)
-console.log('after sort: ', res)
+class File {
+  constructor (name) {
+    this.name = name
+    this.parent = null
+  }
+  
+  add (file) {
+    throw new Error('文件下不能再添加文件')
+  }
+  
+  scan () {
+    console.log(`开始扫描文件：${this.name}`)
+  }
+  
+  remove () {
+    if (!this.parent) return
+    const files = this.parent.files
+    files.forEach((file, index) => {
+      if (file === this) {
+        files.splice(index, 1)
+      }
+    })
+  }
+}
+
+let folder = new Folder('学习资料')
+let folder1 = new Folder('JavaScript')
+let folder2 = new Folder('JQuery')
+
+let file1 = new File('JavaScript设计模式与开发实践')
+let file2 = new File('精通JQuery')
+let file3 = new File('重构与模式')
+
+folder1.add(file1)
+folder2.add(file2)
+
+folder.add(folder1)
+folder.add(folder2)
+folder.add(file3)
+
+folder.scan()
+
+file1.remove()
+
+folder.scan()
